6458f34ee0f556b0a612cd76cf03046ec53247ce
Jared Diaz <Jared.Diaz.1890@gmail.com>
2016-12-05 11:37:30 -0800

    did part 5  and 6

--- project6.js
+++ project6.js
@@ -1,5 +1,11 @@
+var foodCounter = 0;
+
 var CharizardX = 250;
 var CharizardY = 0;
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
@@ -21,9 +27,28 @@ document.addEventListener("keydown", function(e) {
         CharizardY -=15;
  document.getElementById("Charizard").setAttribute("y", CharizardY)
   }
-})
 
-var Food1X=Number(document.getElementById("Food1").getAttribute("x"));
- var Food1y=Number(document.getElementById("Food1").getAttribute("y"));
- var Food2X=Number(document.getElementById("Food2").getAttribute("x"));
- var Food2y=Number(document.getElementById("Food2").getAttribute("y"));
+  var Food1X=Number(document.getElementById("Food1").getAttribute("x"));
+   var Food1y=Number(document.getElementById("Food1").getAttribute("y"));
+   var Food2X=Number(document.getElementById("Food2").getAttribute("x"));
+   var Food2y=Number(document.getElementById("Food2").getAttribute("y"));
+
+  //checking for overlap
+    if (CharizardX > Food1X && CharizardX < Food1X + 20 && CharizardY > Food1y && CharizardY < Food1y + 20) {
+      console.log('overlap');
+      var randX = randomNumber(40, 760)
+      document.getElementById("Food1").setAttribute("x", randX)
+      foodCounter = foodCounter +1;
+      document.getElementById("text1").textContent = foodCounter;
+    }
+
+    if (CharizardY > Food2y && CharizardY < Food2y + 20 && CharizardY > Food2y && CharizardY < Food2y + 20){
+      console.log('overlap');
+      var randX = randomNumber(40, 760)
+      document.getElementById("Food2").setAttribute("x", randX)
+      foodCounter = foodCounter +1;
+      document.getElementById("text1").textContent = foodCounter;
+    }
+
+
+})
